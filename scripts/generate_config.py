13c4e979860eaa6c914558efc072c2bcc4ee7212
Mardzix <bartosovic.marek@gmail.com>
2025-04-22 15:43:44 +0200

    Add -r flag to generate_config.py

--- scripts/generate_config.py
+++ scripts/generate_config.py
@@ -2,9 +2,14 @@
 
 import os, sys
 import glob
+import argparse
 
-path = sys.argv[1]
-extension = "/**/*.fastq.gz"
+parser = argparse.ArgumentParser(description="Generate a config file for the pipeline.")
+parser.add_argument('-i', '--input', type=str, required=True, help='Path to the input directory containing the fastq files.')
+parser.add_argument('-r', '--read_R3_as_R2', action= 'store_true', help='Use this flag to report R3 as R2 if sequenced together with single-cell libraries on the same flowcell')
+parser.add_argument('-e', '--extension', type=str, default='*.fastq.gz', help='File extension of the input files. Default is *.fastq.gz')
+args = parser.parse_args()
+
 
 class seq_file:
   def __init__(self, path):
@@ -45,7 +50,16 @@ def format_config(config_dict):
       print("    {}:".format(lane))
       reads_ls = sorted([curr_files[x].read for x in range(0,len(curr_files)) if curr_files[x].lane == lane])
       for read in reads_ls:
-        print("      {}:".format(read),end = " ")
+        if args.read_R3_as_R2:
+          if read == "R1":
+            read_report = "R1"
+          elif read == "R2":
+            continue
+          elif read == "R3":
+            read_report = "R2"
+        else:
+          read_report = read
+        print("      {}:".format(read_report),end = " ")
         print("  {}".format("\n".join([curr_files[x].path for x in range(0,len(curr_files)) if curr_files[x].lane == lane and curr_files[x].read == read])))
 
 
@@ -54,12 +68,14 @@ def print_general_config():
   print("  lanes:\n    - L001\n    - L002")
   print("  reads:\n    - R1\n    - R2")
   print("  bowtie2_index: FILL_IN_PATH_TO_BOWTIE2_INDEX_HERE")
+
 def main(path, extension):
-  files_list  = [seq_file(x) for x in glob.glob(path + extension,recursive=True)]
+  print(path+extension)
+  files_list  = [seq_file(x) for x in glob.glob(path + "/**/" + extension,recursive=True)]
   config_dict = generate_config_dict(files_list)
   print_general_config()
   format_config(config_dict)
 
-main(path, extension)
+main(args.input, args.extension)
 
 
